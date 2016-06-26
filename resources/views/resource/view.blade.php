89a3afdcbf820cc5dc4061af1f988574dabba3ad
Dustin Spears <spearsd@etsu.edu>
2016-06-25 23:04:36 -0400

    Addition files I failed to stage for the view resource page.

--- resources/views/resource/view.blade.php
+++ resources/views/resource/view.blade.php
@@ -5,50 +5,41 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Delete</div>
-
+                    <div class="panel-heading">View Resource</div>
                     <div class="panel-body">
-                        <form class="form-horizontal" method="post" action="/resource/destroy/{{$id->Id}}" accept-charset="UTF-8">
-                            {{ csrf_field() }}
-                            {{ method_field('DELETE') }}
-                            <dl class="dl-horizontal">
-                                <dt>Name</dt>
-                                <dd>{{ $id->Name }}</dd>
-                                <dt>StreetAddress</dt>
-                                <dd>{{ $id->StreetAddress }}</dd>
-                                <dt>StreetAddress2</dt>
-                                <dd>{{ $id->StreetAddress2 }}</dd>
-                                <dt>City</dt>
-                                <dd>{{ $id->City }}</dd>
-                                <dt>County</dt>
-                                <dd>{{ $id->County }}</dd>
-                                <dt>State</dt>
-                                <dd>{{ $id->State }}</dd>
-                                <dt>Zipcode</dt>
-                                <dd>{{ $id->Zipcode }}</dd>
-                                <dt>ContactName</dt>
-                                <dd>{{ $id->ContactName }}</dd>
-                                <dt>ContactPhone</dt>
-                                <dd>{{ $id->ContactPhone }}</dd>
-                                <dt>OpeningHours</dt>
-                                <dd>{{ $id->OpeningHours }}</dd>
-                                <dt>ClosingHours</dt>
-                                <dd>{{ $id->ClosingHours }}</dd>
-                                <dt>Comments</dt>
-                                <dd>{{ $id->Comments }}</dd>
-                            </dl>
-                            <div class="col-lg-6"><br/><br/>
-                                <div class="text-center">Delete <b>{{ $id->Name  }}</b> from the database?</br></br>
-                                    <div class="col-md-5 text-center">
-                                        <input class="btn btn-primary" type="submit" value="Yes!">
-                                    </div>
-                                </div>
+                        <dl class="dl-horizontal">
+                            <dt>Name</dt>
+                            <dd>{{ $id->Name }}</dd>
+                            <dt>StreetAddress</dt>
+                            <dd>{{ $id->StreetAddress }}</dd>
+                            <dt>StreetAddress2</dt>
+                            <dd>{{ $id->StreetAddress2 }}</dd>
+                            <dt>City</dt>
+                            <dd>{{ $id->City }}</dd>
+                            <dt>County</dt>
+                            <dd>{{ $id->County }}</dd>
+                            <dt>State</dt>
+                            <dd>{{ $id->State }}</dd>
+                            <dt>Zipcode</dt>
+                            <dd>{{ $id->Zipcode }}</dd>
+                            <dt>ContactName</dt>
+                            <dd>{{ $id->ContactName }}</dd>
+                            <dt>ContactPhone</dt>
+                            <dd>{{ $id->ContactPhone }}</dd>
+                            <dt>OpeningHours</dt>
+                            <dd>{{ $id->OpeningHours }}</dd>
+                            <dt>ClosingHours</dt>
+                            <dd>{{ $id->ClosingHours }}</dd>
+                            <dt>Comments</dt>
+                            <dd>{{ $id->Comments }}</dd>
+                        </dl>
+                        <div class="col-lg-6"><br/><br/>
+                            <div class="text-center"><a href="{{'/home'}}">Go Back to Home Page</a></br></br>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     </div>
 @endsection

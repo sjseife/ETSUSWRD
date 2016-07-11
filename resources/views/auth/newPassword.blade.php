8fec4714e42e4e07e3eb7298d686cb613240f204
Matthew Humphrey <humphreymg@goldmail.etsu.edu>
2016-07-11 05:59:30 -0400

    Fixing my version, if you see this DELETE it

--- resources/views/auth/newPassword.blade.php
+++ resources/views/auth/newPassword.blade.php
@@ -5,8 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Reset Password</div>
-
+                    <div class="panel-heading">Change Password</div>
                     <div class="panel-body">
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/newPassword') }}">
                             {{ csrf_field() }}

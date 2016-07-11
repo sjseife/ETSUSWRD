3f6b2af8aedc49153b701e15be4cd6d4a845e9cb
Matthew Humphrey <humphreymg@goldmail.etsu.edu>
2016-07-11 10:16:12 -0400

    Fixing my version, if you see this DELETE it

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,6 +19,8 @@ Route::auth();
 
 Route::get('/home', 'HomeController@index');
 
+Route::get('/auth/newPassword', 'HomeController@index');
+
 Route::get('/team', function () {
     return view('team');
 });

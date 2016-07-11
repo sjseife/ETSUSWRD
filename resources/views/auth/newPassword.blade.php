6d412f3d89df1269e298c1dad5220d12ba22560f
Matthew Humphrey <humphreymg@goldmail.etsu.edu>
2016-07-11 11:14:30 -0400

    Fixing my version, if you see this DELETE it

--- resources/views/auth/newPassword.blade.php
+++ resources/views/auth/newPassword.blade.php
@@ -11,19 +11,6 @@
                             {{ csrf_field() }}
 
                             {{--<input type="hidden" name="token" value="{{ $token }}">--}}
-                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}">
-
-                                    @if ($errors->has('email'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
 
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                                 <label for="password" class="col-md-4 control-label">Current Password</label>

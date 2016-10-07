b5fb8f89076b1710bbd6b6266aa86d300bb6e796
Hookeo <berkleyr@etsu.edu>
2016-10-07 12:30:56 -0400

    no message

--- resources/views/Resources/show.blade.php
+++ resources/views/Resources/show.blade.php
@@ -3,82 +3,92 @@
 @section('content')
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">View Resource</div>
-                <div class="panel-body">
+
+                        <h2><em>{{ $resource->Name }}</em></h2>
+            <div class="col-md-4">
+                <p><b>Address:</b></p>
+                {{--If there is a street 2, display. Else do not--}}
+                @if($resource->StreedAddress2 != null)
+                    <p>{{ $resource->StreetAddress }}<br>
+                        {{ $resource->StreetAddress2 }}</p>
+                @else
+                    <p>{{ $resource->StreetAddress }}</p>
+                @endif
+
+                <p>{{ $resource->City }}, {{ $resource->State }} {{ $resource->Zipcode }}</p>
+                <p>{{ $resource->County }}</p>
+            </div>
+            <div class="col-md-2">
+                <p><b>Hours:</b></p>
+                <p>{{ $resource->OpeningHours }} - {{ $resource->ClosingHours }}</p>
+            </div>
+            <div class="col-md-4">
+                <p><b>Contacts:</b></p>
+                @foreach($resource->contacts as $contact)
+                    <p>
+                        <a href="{{ URL::to('contacts/' . $contact->id) }}">
+                            {{ $contact->full_name }}
+                        </a>
+                    </p>
+                @endforeach
+            </div>
+            <div class="col-md-10">
+                <hr />
+                <p><b>Description:</b></p>
+                <p>Lorem ipsum dolor sit amet, qui malis vituperatoribus an, hinc tractatos facilisis an sit. Te est
+                    diceret sanctus, an movet commune lobortis qui. Nobis maluisset intellegat sea ad, est liber dolor
+                    in. Vel cu quodsi doctus, te eirmod deleniti eam. Mei id rebum nemore corpora, sit modo debitis
+                    accumsan in. Ei mea quas discere, error debitis eu qui, quem antiopam dignissim id vim. Mea ut
+                    aperiam accusamus.</p>
+
+                <p><b>Categories</b></p>
+                <p>
+                @foreach ($resource->categories as $category)
+                        <a href="{{ URL::to('categories/' . $category->id) }}">
+                           | {{ $category->name }}
+                        </a>
+                @endforeach
+                | </p>
+                <hr />
+                <p><b>Comments:</b></p>
+                <p>{{ $resource->Comments }}</p>
+                <hr />
+            </div>
+            <div class="col-md-10">
+                <dt>Reported Problems</dt>
+                @if(isset($resource->flags))
+                    @foreach($resource->flags as $flag)
+                        @if(!$flag->resolved)
+                            <dd>{{ $flag->comments }}</dd>
+                        @endif
+                    @endforeach
+                @else
+                    <dd>No problems reported</dd>
+                @endif
+                <div class="col-md-10">
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <!-- edit this resource (uses the edit method found at GET /resource/edit/{id} -->
+                    <a class="btn btn-lg btn-info" href="{{ URL::to('resources/' . $resource->id. '/edit') }}">Edit</a>
+                    <!-- delete the resource -->
+                    <!-- Trigger the modal with a button -->
+                    <button type="button" class="btn btn-warning btn-lg" data-toggle="modal" data-target="#deleteModal">Delete</button>
                     <div class="col-md-offset-2"><br/><br/>
                         <div>
-                            <a href="{{'/resources'}}">Back to Resources</a></br></br>
+                            <a href="{{'/resources'}}">Back to Resources</a></br>
+
+                            <!-- Flag this resource as incorrect -->
+                            <a  href="{{ URL::to('resources/' . $resource->id. '/flag') }}">Report a problem with this resource.</a>
                         </div>
                     </div>
-                    <dl class="dl-horizontal">
-                        <dt>Name</dt>
-                        <dd>{{ $resource->Name }}</dd>
-                        <dt>Street Address</dt>
-                        <dd>{{ $resource->StreetAddress }}</dd>
-                        <dt>Street Address 2</dt>
-                        <dd>{{ $resource->StreetAddress2 }}</dd>
-                        <dt>City</dt>
-                        <dd>{{ $resource->City }}</dd>
-                        <dt>County</dt>
-                        <dd>{{ $resource->County }}</dd>
-                        <dt>State</dt>
-                        <dd>{{ $resource->State }}</dd>
-                        <dt>Zip code</dt>
-                        <dd>{{ $resource->Zipcode }}</dd>
-                        <dt>Opening Hours</dt>
-                        <dd>{{ $resource->OpeningHours }}</dd>
-                        <dt>Closing Hours</dt>
-                        <dd>{{ $resource->ClosingHours }}</dd>
-                        <dt>Comments</dt>
-                        <dd>{{ $resource->Comments }}</dd>
-                        <dt>Contacts</dt>
-                        @foreach($resource->contacts as $contact)
-                            <dd>
-                                <a href="{{ URL::to('contacts/' . $contact->id) }}">
-                                    {{ $contact->full_name }}
-                                </a>
-                            </dd>
-                        @endforeach
-                        <dt>Categories</dt>
-                        @foreach ($resource->categories as $category)
-                            <dd>
-                                <a href="{{ URL::to('categories/' . $category->id) }}">
-                                    {{ $category->name }}
-                                </a>
-                            </dd>
-                        @endforeach
-                        <dt>Reported Problems</dt>
-                        @if(isset($resource->flags))
-                            @foreach($resource->flags as $flag)
-                                @if(!$flag->resolved)
-                                <dd>{{ $flag->comments }}</dd>
-                                @endif
-                            @endforeach
-                        @else
-                            <dd>No problems reported</dd>
-                        @endif
-
-                    </dl>
-                    <div class="col-md-offset-2">
-                        <br/>
-                        <br/>
-                        <br/>
-                        <!-- Flag this resource as incorrect -->
-                        <a class="btn btn-lg btn-link" href="{{ URL::to('resources/' . $resource->id. '/flag') }}">Report a problem with this resource.</a>
-                        <br/>
-                        <br/>
-                        <!-- edit this resource (uses the edit method found at GET /resource/edit/{id} -->
-                        <a class="btn btn-lg btn-info" href="{{ URL::to('resources/' . $resource->id. '/edit') }}">Edit</a>
-                        <!-- delete the resource -->
-                        <!-- Trigger the modal with a button -->
-                        <button type="button" class="btn btn-warning btn-lg" data-toggle="modal" data-target="#deleteModal">Delete</button>
-                        <br/>
-                        <br/>
-                    </div>
+                    <br/>
+                    <br/>
                 </div>
             </div>
-        </div>
+
     </div>
 
     <!-- Modal -->

6b4c0c53fe64aefa30573d146be6ec8ab9051d44
Dustin_Spears <spearsd@etsu.edu>
2016-10-15 21:33:18 -0400

    Added mobile detect package to composer, report page is now displayed differently for mobile and desktop browsers.

--- resources/views/WorkList/generateReport.blade.php
+++ resources/views/WorkList/generateReport.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.general')
 
 @section('content')
-
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <style>
         .table-nonfluid {
             width: 40% !important;
@@ -10,69 +10,105 @@
             color: red !important;
         }
     </style>
-
+<?php
+    $detect = new Mobile_Detect;
+?>
     @if($resourcesSet != false)
         <h1 class="text-center">Resource and Event Report</h1>
-        <div class="container">
             <div class="container">
                 <a class="btn btn-small btn-danger pull-left" href="{{ URL::to('worklist/emptyReport') }}" style="margin-bottom: 20px;">Empty Report</a>
+                <?php if ( $detect->isMobile() ) { ?>
+                    <a class="btn btn-small btn-success pull-right" target="_blank" href="{{ URL::to('/report.pdf') }}" style="margin-bottom: 20px;">Download</a>
+                <?php }
+                else{ ?>
                 <a class="btn btn-small btn-info pull-right" target="_blank" href="{{ URL::to('worklist/generatePDF') }}" style="margin-bottom: 20px;">External Preview</a>
+                <?php } ?>
             </div>
-            <div>
-                <table align="left" class="table table-striped table-nonfluid table-bordered dt-responsive nowrap" cellspacing="0" width="100%" id="ReportTable">
-                    @if(!$events->isEmpty())
-                        <thead>
-                        <tr>
-                            <!-- class all for always show, lower data priority numbers stay longer-->
-                            <th class="all">Event Name</th>
-                            <th class="all">Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($events as $key => $event)
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <table align="left" class="table table-striped table-bordered" id="ReportTable">
+                        @if(!$events->isEmpty())
+                            <thead>
                             <tr>
-                                <td>{{ $event->name }}</td>
-                                <td class="text-center col-small-3">
-                                    <a href="/events/removeReport/{{$event->id}}" role="button" class="btn-link btn-sm removeReport
-                                        @if(!Auth::user()->events->contains($event))
-                                            disabled
-                                        @endif
-                                            ">Remove from Report</a>
-                                </td>
+                                <!-- class all for always show, lower data priority numbers stay longer-->
+                                <th class="all">Event Name</th>
+                                <th class="all">Action</th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    @endif
-                    @if(!$resources->isEmpty())
-                        <thead>
-                        <tr>
-                            <!-- class all for always show, lower data priority numbers stay longer-->
-                            <th class="all">Resource Name</th>
-                            <th class="all">Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($resources as $key => $resource)
+                            </thead>
+                            <tbody>
+                            @foreach($events as $key => $event)
+                                <tr>
+                                    <td>{{ $event->name }}</td>
+                                    <?php
+                                    if ( $detect->isMobile() ) { ?>
+                                        <td class="text-center col-small-3">
+                                            <a href="/events/removeReport/{{$event->id}}" role="button" class="btn-link btn-sm removeReport
+                                            @if(!Auth::user()->events->contains($event))
+                                                    disabled
+                                                @endif
+                                                    ">Remove</a>
+                                        </td>
+                                    <?php }
+                                    else{ ?>
+                                        <td class="text-center col-small-3">
+                                            <a href="/events/removeReport/{{$event->id}}" role="button" class="btn-link btn-sm removeReport
+                                            @if(!Auth::user()->events->contains($event))
+                                                    disabled
+                                                @endif
+                                                    ">Remove from Report</a>
+                                        </td>
+                                    <?php } ?>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        @endif
+                        @if(!$resources->isEmpty())
+                            <thead>
                             <tr>
-                                <td>{{ $resource->name }}</td>
-                                <td class="text-center col-small-3">
-                                    <a href="/resources/removeReport/{{$resource->id}}" role="button" class="btn-link btn-sm removeReport
-                                        @if(!Auth::user()->resources->contains($resource))
-                                            disabled
-                                        @endif
-                                            ">Remove from Report</a>
-                                </td>
+                                <!-- class all for always show, lower data priority numbers stay longer-->
+                                <th class="all">Resource Name</th>
+                                <th class="all">Action</th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    @endif
-                </table>
-                <div class="container text-right">
-                    <object data="/report.pdf" type="application/pdf" width="60%" height="700">
-                    </object>
+                            </thead>
+                            <tbody>
+                            @foreach($resources as $key => $resource)
+                                <tr>
+                                    <td>{{ $resource->name }}</td>
+                                    <?php
+                                    if ( $detect->isMobile() ) { ?>
+                                    <td class="text-center col-small-3">
+                                        <a href="/resources/removeReport/{{$resource->id}}" role="button" class="btn-link btn-sm removeReport
+                                            @if(!Auth::user()->resources->contains($resource))
+                                                disabled
+                                            @endif
+                                                ">Remove</a>
+                                    </td>
+                                    <?php }
+                                    else{ ?>
+                                    <td class="text-center col-small-3">
+                                        <a href="/resources/removeReport/{{$resource->id}}" role="button" class="btn-link btn-sm removeReport
+                                            @if(!Auth::user()->resources->contains($resource))
+                                                disabled
+                                            @endif
+                                                ">Remove from Report</a>
+                                    </td>
+                                    <?php } ?>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        @endif
+                    </table>
                 </div>
+                <?php if ( !$detect->isMobile() ) { ?>
+                    <div class="col-md-7">
+                        <object data="/report.pdf" type="application/pdf" width="100%" height="700">
+                        </object>
+                    </div>
+                <?php } ?>
             </div>
         </div>
+
     @else
         <h1 class="text-center">Resource and Event Report</h1>
         <br>

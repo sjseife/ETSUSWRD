0b71495813523d06b0f01597551c790a0c02130e
rich <nguyenr@goldmail.etsu.edu>
2016-10-26 11:17:04 -0400

    User cancel button

--- resources/views/Users/_form.blade.php
+++ resources/views/Users/_form.blade.php
@@ -32,5 +32,7 @@
     <div class="col-md-2"></div>
     <div class="col-md-4">
         {!! Form::submit($submitButtonText, ['class' => 'btn btn-primary']) !!}
+        <a href="{{ '/users' }}" class="btn btn-danger"  style="margin-left:10%">Cancel</a>
+
     </div>
 </div>

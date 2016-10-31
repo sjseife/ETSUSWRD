b007e1346ed00f57ee736fd1940679ae2579d4ac
Dustin_Spears <spearsd@etsu.edu>
2016-10-31 18:41:26 -0400

    Reformatted contact view to reflect the views of resources and events.

--- resources/views/Contacts/show.blade.php
+++ resources/views/Contacts/show.blade.php
@@ -1,31 +1,23 @@
-@extends('layouts.general')
-
+@extends('layouts.dataTables')
 @section('content')
     <div class="row">
-        <div class="col-md-6 col-md-offset-3">
+        <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-primary">
                 <div class="panel-heading">View Contact</div>
-                <div class="panel-body">
+                <div class="panel-body ">
                     <a href="{{ '/contacts' }}" class="btn btn-default">Back</a>
-
-                    <div class="col-md-offset"><br/><br/>
-
+                    <h2><em>{{ $contact->firstName}}&nbsp; {{$contact->lastName }}</em></h2>
+                    <div class="col-md-4">
+                        <p><b>Email:</b></p>
+                        {{ $contact->protectedEmail }}
                     </div>
-
-                    <br/>
-
-                        <h2>&nbsp;&nbsp;{{ $contact->firstName}}&nbsp; {{$contact->lastName }}</h2>
-                        <div class="col-md-10">
-
-
-                            <p>&nbsp;&nbsp;Email: &nbsp; {{ $contact->protectedEmail }}</p>
-
-                            <p>&nbsp;&nbsp;Phone: &nbsp; {{ $contact->protectedPhoneNumber }}</p>
-
-                            <hr>
-                        </div>
-                        <div class="col-md-10">
-                            <p><strong>Providers</strong></p>
+                    <div class="col-md-4">
+                        <p><b>Phone Number:</b></p>
+                        {{ $contact->protectedPhoneNumber }}
+                    </div>
+                    <div class="col-md-4">
+                        <p><strong>Providers:</strong></p>
+                        @if(!$contact->providers->isEmpty())
                             @foreach($contact->providers as $provider)
                                 <p>
                                     <a href="{{ URL::to('providers/' . $provider->id) }}">
@@ -33,46 +25,52 @@
                                     </a>
                                 </p>
                             @endforeach
-                            <hr>
-                        </div>
-                        <div class="col-md-10">
-                            <p>Reported Problems</p>
-                            @if(isset($contact->flags))
-                                @foreach($contact->flags as $flag)
-                                    @if(!$flag->resolved)
-                                        <p>{{ $flag->comments }}</p>
-                                    @endif
-                                @endforeach
-                            @else
-                                <p>No problems reported</p>
-                            @endif
-                        </div>
-                        <div class="col-md-10">
-                            <br/>
-                            <br/>
-                            <br/>
-
-                            <div class="col-md-offset-2">
-                                <!-- edit this contact (uses the edit method found at GET /contact/{id}/edit -->
-                                <a class="btn btn-lg btn-info" href="{{ URL::to('contacts/' . $contact->id. '/edit') }}">Edit</a>
-                                <!-- delete the resource -->
-                                <!-- Trigger the modal with a button -->
-                                <button type="button" class="btn btn-warning btn-lg" data-toggle="modal" data-target="#deleteModal">Delete</button>
-                                <br/>
-                                <br/>
-                                <div>
-
-                                    <!-- Flag this provider as incorrect -->
-                                    <a  href="{{ URL::to('contacts/' . $contact->id. '/flag') }}">Report a problem with this contact.</a>
-                                </div>
+                        @else
+                            <p>N/A</p>
+                        @endif
+                    </div>
+                    <div class="col-md-10">
+                        <hr/>
+                    </div>
+                    <div class="col-md-10">
+                        <p><b>Reported Problems:</b></p>
+                        @if(!$contact->flags->isEmpty())
+                            @foreach($contact->flags as $flag)
+                                @if(!$flag->resolved)
+                                    <p>{{ $flag->comments }}</p>
+                                @endif
+                            @endforeach
+                        @else
+                            <p>No problems reported</p>
+                        @endif
+                    </div>
+                    <div class="col-md-10">
+                        <hr/>
+                    </div>
+                    <div class="col-md-10 col-md-offset-4">
+                        <br/>
+                        @if (Auth::user()->role == 'GA' || Auth::user()->role == 'Admin')
+                        <!-- edit this contact (uses the edit method found at GET /resource/edit/{id} -->
+                            <a class="btn btn-md btn-info" href="{{ URL::to('contacts/' . $contact->id. '/edit') }}">Edit</a> |
+                            <!-- delete the contact -->
+                            <!-- Trigger the modal with a button -->
+                            <button type="button" class="btn btn-warning btn-md" data-toggle="modal" data-target="#deleteModal">Delete</button>
+                        @endif
+                        <br/>
+                        <br/>
+                        <div class=""><br/><br/>
+                            <div>
+                                <!-- Flag this contact as incorrect -->
+                                <a  href="{{ URL::to('contacts/' . $contact->id. '/flag') }}" class="btn btn-danger">Report a problem with this contact.</a>
                             </div>
-
                         </div>
+                    </div>
+                    <br/>
+                    <br/>
+                </div>
             </div>
         </div>
     </div>
-        </div>
-
     <!-- Modal -->
     @include('contacts._deleteModal')
 @stop

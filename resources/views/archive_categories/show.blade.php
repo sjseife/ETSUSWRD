5dde22d868be31f0c557303556f0f4d7d6c423bc
Stan Seiferth <zsjs19@goldmail.etsu.edu>
2016-11-18 13:20:48 -0500

    changed back link to a button

--- resources/views/archive_categories/show.blade.php
+++ resources/views/archive_categories/show.blade.php
@@ -6,7 +6,7 @@
             <div class="panel panel-primary">
                 <div class="panel-heading">View Category</div>
                 <div class="panel-body">
-                    <a href="{{ url('/flags') }}">Back</a></br></br>
+                    <a href="{{ url('/archive_categories') }}" class="btn btn-default">Back</a>
                     <div class="col-md-offset-2"><br/><br/>
 
                     </div>
